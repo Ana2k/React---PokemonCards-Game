99c63c8e82269e4f6b6fa26990b1f4068e6aca92
Ana2k <anushkachakra@gmail.com>
2022-05-26 16:42:29 +0530

    testing

---
+++ src/App.js
@@ -0,0 +1,16 @@
+    import React,{Component} from 'react';
+    import './App.css';
+    import Pokecard from './Pokecard'
+// #run 1 
+    function App() {
+    return (
+        <div className="App">
+        <Pokecard/>
+        </div>
+    );
+    }
+
+    export default App;
+        {/* we could go from the pokedex to the pokecard */}
+      {/* {/* or from the pokecard to the pokedex */ }
+    {/* //   we are going to go from the pokecard to pokedex */}
---
+++ src/Pokecard.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+
+class Pokecard extends Component{
+    render(){
+        return <h1>Pokecard HERE!</h1>
+    }
+}
+export default Pokecard;
