27ac2d2b6b91dbccafc0f40d32007104229bd8fd
Ana2k <anushkachakra@gmail.com>
2022-05-28 02:21:26 +0530

    array of pokemons :) change this list to change pokemons ;)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
     // instructions page file:///home/ana/Desktop/reactjs/react-bootcamp-materials/05-pokedex-project/pokedex-exericse-handout/index.html
     import React,{Component} from 'react';
     import './App.css';
-    import Pokecard from './Pokecard'
+    import Pokedex from './Pokedex'
 // #run 1 
     function App() {
     return (
         <div className="App">
-        <Pokecard id={4} name="Charmendar" type="fire" exp={62} />
-        {/* these are all just props bruh so you have to access them in the pokecard.js react app part */}
+        <Pokedex />
         </div>
     );
     }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import './Pokecard.css'
 // API CALLING 
 //we will put up nicer images later.
 
