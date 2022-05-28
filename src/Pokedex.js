cb53dafcbc1d039921c58c7b9cae8bde77fce2d1
Ana2k <anushkachakra@gmail.com>
2022-05-28 18:18:21 +0530

    csss styling

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,6 +1,6 @@
-import React,{Component} from 'react'
+import React,{Component} from 'react';
 import Pokecard from './Pokecard';
-import './Pokedex.css'
+import './Pokedex.css';
 
 class Pokedex extends Component{
     static defaultProps = {
@@ -19,7 +19,7 @@ class Pokedex extends Component{
         return (
         <div className="Pokedex">
             <h1>Pokedex!</h1>
-            <div className='Pokedex-Cards'>
+            <div className='Pokedex-cards'>
             {this.props.pokemon.map( (p)=>(
                 <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience}/>
             ))}
